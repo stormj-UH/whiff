d37adf0ed9386d8d0f6e5341034a696814f601de
stormj-UH <74021132+stormj-UH@users.noreply.github.com>
2021-12-11 10:39:14 -0800

    added copyright

--- whiff.sh
+++ whiff.sh
@@ -1,3 +1,19 @@
+# Whiff - a better which for when which whiffs
+# Copyright (C) 2021 Jon-Erik G. Storm
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #!/bin/sh/
 # Load the command line parameters
 # Look for options
