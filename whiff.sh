b2c03784f2b5d64430ecbfffbcaa2898348f0df1
stormj-UH <74021132+stormj-UH@users.noreply.github.com>
2021-12-11 10:36:17 -0800

    Update whiff.sh

--- whiff.sh
+++ whiff.sh
@@ -1,2 +1,16 @@
 #!/bin/sh/
-# kode
+# Load the command line parameters
+# Look for options
+# Iterate through the non hyphenated parameters
+# # If PATH is null, look in the working directory
+# # If working directory finds nothing, search file system unless -p
+# # # If file system finds nothing, say "None found." unless -s then return 0
+# # # If found in filesystem, print long path of it and keep looking through the file system
+# # If PATH is not null, look in the working directory, then look at each PATH directory one by one
+# # # If nothing found in PATH, search file system unless -p
+# # # # If nothing found in file system, say "None found." unless -s then return 0
+# # # # If file is found, print long path and keep looking through the file system
+# # # If found in PATH, say so. 
+# # # If -a is in place, keep searching PATH.
+# Done
+
