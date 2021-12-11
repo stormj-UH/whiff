e9c1418976a5a388b6e65d04877570b6f626d5bb
Jon-Erik Storm <jonerik@m1ni.local>
2021-12-11 12:34:24 -0800

    Mockup working

--- whiff.sh
+++ whiff.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 # Whiff - a better which for when which whiffs
 # Copyright (C) 2021 Jon-Erik G. Storm
 #
@@ -14,7 +15,87 @@
 #    You should have received a copy of the GNU General Public License along
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-#!/bin/sh/
+function findFile()
+{
+
+}
+function searchPath()
+{
+    if [ !$SINGLE_FLAG ]
+    then
+        for i in $PATH
+        do
+        if [ -n findFile($i, $file) ]
+        then
+            echo $dir, $file
+        done
+    else
+        IFS=;
+        for i in $PATH
+        do
+        # look for file then break
+        if [ -n findFile($i, $file) ]
+        then
+            echo $i, $file
+            exit
+        done
+    fi
+}
+function searchSystem()
+{
+
+}
+function parseCommandLine()
+{
+    for i in $@
+    do
+       case i
+            -a)
+                SINGLE_FLAG = FALSE
+            -s)
+                NO_OUTPUT_FLAG = TRUE
+            -p)
+                PATH_ONLY_FLAG = TRUE
+            *)
+                i = ${program}$i
+                j = j + 1
+       esac        
+}
+if [ -z "$1" ]
+then
+   echo "No parameters specified, displaying help."
+   echo "Usage: whiff [-asp] \n \
+   -a : List all instances of executables found (instead of just the first one of each).\n \
+   -s : No output, just return 0 if all of the executables are found, or 1 if some were not found.\n \
+   -p : Limit search to \$PATH, just like which." 
+fi
+parseCommandLine()
+if [ -z $PATH ]
+then
+    findFile(".")
+    if [ !$PATH_ONLY_FLAG]
+    then
+        searchSystem()
+    fi
+else
+    searchPath()
+    if [ !$PATH_ONLY_FLAG]
+    then
+        searchSystem()
+    fi
+fi
+if [ $NO_OUTPUT_FLAG ]
+then
+    echo $RESULTS
+else
+    # if number of results doesn't equal number of executables sought
+    then
+        return 1
+    else
+        return 0
+    fi
+fi
+exit
 # Load the command line parameters
 # Look for options
 # Iterate through the non hyphenated parameters
